9c21d2218e477e6d69008be0584ca4534adf27e4
csainah <sainahcorneli92@gmail.com>
2024-07-20 16:14:38 +0300

    Create grades.js

---
+++ Student Grade Generator/grades.js
@@ -0,0 +1,12 @@
+function CalculateGrade (mark) {
+  if (mark>79){
+    return 'A';
+  } else if (mark>=60){
+    return 'B';
+  } else if (mark>=50){
+    return 'C';
+  } else if (mark>=40){
+    return 'D';
+  } else{
+    return 'E';
+  }
